b7dd263286c703a8e130616c9eceedc7e501a2a7
Darren <jesn2013@hotmail.com>
2022-05-10 10:55:46 +0800

    修改dockerfile

---
+++ src/Wechaty.OpenApi.HttpApi/Handler/ContactMessageHub.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Volo.Abp.AspNetCore.SignalR;
+using Wechaty.Module.Puppet.Schemas;
+using Wechaty.OpenApi.Wechaty;
+
+namespace Wechaty.OpenApi.Handler
+{
+    public class ContactMessageHub : AbpHub
+    {
+        private readonly IMessageAppService _messageAppService;
+
+        public ContactMessageHub(IMessageAppService messageAppService)
+        {
+            _messageAppService = messageAppService;
+        }
+
+        /// <summary>
+        /// 发送文本
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public async Task<string> SendText(SendTextInput input)
+        {
+            var text = await _messageAppService.MessageSendTextAsync(input);
+            return text;
+        }
+
+        /// <summary>
+        /// 发送小程序
+        /// </summary>
+        /// <param name="conversationId"></param>
+        /// <param name="miniProgramPayload"></param>
+        /// <returns></returns>
+        public async Task SendMiniProgram(string conversationId, MiniProgramPayload miniProgramPayload)
+        {
+            await _messageAppService.MessageSendMiniProgramAsync(conversationId, miniProgramPayload);
+        }
+    }
+}
